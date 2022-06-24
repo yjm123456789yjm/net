64b7da895a29fc8015c3e269abe67764d59c4146
yjm <yjm123456789yjm@outlook.com>
2022-06-24 21:26:00 +0800

    text_2

--- Program.cs
+++ Program.cs
@@ -29,3 +29,5 @@
 // Console.WriteLine("Fourth: " + (++value));
 
 int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+Console.WriteLine("The temperature is " + celsius + " Celsius.");
