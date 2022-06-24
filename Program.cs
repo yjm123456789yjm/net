f8720c51625e3c54ab7fe238d281d1cafc60b8a4
yjm <yjm123456789yjm@outlook.com>
2022-06-25 07:36:47 +0800

    text_4

--- Program.cs
+++ Program.cs
@@ -1,56 +1,22 @@
-﻿// // See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, C# World!");
+﻿using System.Diagnostics;
+int result = Fibonacci(5);
+Console.WriteLine(result);
 
-// int value = 1;
+static int Fibonacci(int n)
+{
+    Debug.WriteLine($"Enter {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {n}th number");
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
 
-// value = value + 1;
-// Console.WriteLine("First increment: " + value);
+    for (int i = 2; i <= n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+        Debug.WriteLineIf(sum==1, $"sum is 1, n1 is {n1}, n2 is {n2}");
+    }
 
-// value += 1;
-// Console.WriteLine("Second increment: " + value);
-
-// value++;
-// Console.WriteLine("Third increment: " + value);
-
-// value = value - 1;
-// Console.WriteLine("First decrement: " + value);
-
-// value -= 1;
-// Console.WriteLine("Second decrement: " + value);
-
-// value--;
-// Console.WriteLine("Third decrement: " + value);
-
-// int value = 1;
-// value++;
-// Console.WriteLine("First: " + value);
-// Console.WriteLine("Second: " + value++);
-// Console.WriteLine("Third: " + value);
-// Console.WriteLine("Fourth: " + (++value));
-
-// int fahrenheit = 94;
-// decimal celsius = (fahrenheit - 32m) * (5m / 9m);
-// Console.WriteLine("The temperature is " + celsius + " Celsius.");
-
-// int result = Fibonacci(5);
-// Console.WriteLine(result);
-
-// static int Fibonacci(int n)
-// {
-//     int n1 = 0;
-//     int n2 = 1;
-//     int sum;
-
-//     for (int i = 2; i <= n; i++)
-//     {
-//         sum = n1 + n2;
-//         n1 = n2;
-//         n2 = sum;
-//     }
-
-//     return n == 0 ? n1 : n2;
-// }
-
-Console.WriteLine("This message is readable by the end user.");
-// Trace.WriteLine("This is a trace message when tracing the app.");
-// Debug.WriteLine("This is a debug message just for developers.");
+    return n == 0 ? n1 : n2;
+}
