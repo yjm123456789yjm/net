b765fbbaaa25d192c91672e3b20ebd9732f83ae8
yjm <yjm123456789yjm@outlook.com>
2022-06-24 17:48:24 +0800

    yes_text_1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
 ﻿// // See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, C# World!");
 
+// int value = 1;
+
+// value = value + 1;
+// Console.WriteLine("First increment: " + value);
+
+// value += 1;
+// Console.WriteLine("Second increment: " + value);
+
+// value++;
+// Console.WriteLine("Third increment: " + value);
+
+// value = value - 1;
+// Console.WriteLine("First decrement: " + value);
+
+// value -= 1;
+// Console.WriteLine("Second decrement: " + value);
+
+// value--;
+// Console.WriteLine("Third decrement: " + value);
+
+// int value = 1;
+// value++;
+// Console.WriteLine("First: " + value);
+// Console.WriteLine("Second: " + value++);
+// Console.WriteLine("Third: " + value);
+// Console.WriteLine("Fourth: " + (++value));
+
+int fahrenheit = 94;
