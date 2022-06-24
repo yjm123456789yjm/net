e87333e59cfcebff0cfcfc039a67620cfaace9d9
yjm <yjm123456789yjm@outlook.com>
2022-06-25 07:44:54 +0800

    text_5

--- Program.cs
+++ Program.cs
@@ -1,22 +1,23 @@
-﻿using System.Diagnostics;
-int result = Fibonacci(5);
-Console.WriteLine(result);
+﻿// using System.Diagnostics;
+// int result = Fibonacci(5);
+// Console.WriteLine(result);
 
-static int Fibonacci(int n)
-{
-    Debug.WriteLine($"Enter {nameof(Fibonacci)} method");
-    Debug.WriteLine($"We are looking for the {n}th number");
-    int n1 = 0;
-    int n2 = 1;
-    int sum;
+// static int Fibonacci(int n)
+// {
+//     Debug.WriteLine($"Enter {nameof(Fibonacci)} method");
+//     Debug.WriteLine($"We are looking for the {n}th number");
+//     int n1 = 0;
+//     int n2 = 1;
+//     int sum;
 
-    for (int i = 2; i <= n; i++)
-    {
-        sum = n1 + n2;
-        n1 = n2;
-        n2 = sum;
-        Debug.WriteLineIf(sum==1, $"sum is 1, n1 is {n1}, n2 is {n2}");
-    }
+//     for (int i = 2; i <= n; i++)
+//     {
+//         sum = n1 + n2;
+//         n1 = n2;
+//         n2 = sum;
+//         Debug.WriteLineIf(sum==1, $"sum is 1, n1 is {n1}, n2 is {n2}");
+//     }
+//     Debug.Assert(n2==5, "The return value is not 5 and it should be.");
+//     return n == 0 ? n1 : n2;
+// }
 
-    return n == 0 ? n1 : n2;
-}
