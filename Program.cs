f510ec1da2769a4a14e35cb6329fe008881f9387
yjm <yjm123456789yjm@outlook.com>
2022-06-25 07:01:17 +0800

    text_3

--- Program.cs
+++ Program.cs
@@ -28,6 +28,29 @@
 // Console.WriteLine("Third: " + value);
 // Console.WriteLine("Fourth: " + (++value));
 
-int fahrenheit = 94;
-decimal celsius = (fahrenheit - 32m) * (5m / 9m);
-Console.WriteLine("The temperature is " + celsius + " Celsius.");
+// int fahrenheit = 94;
+// decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+// Console.WriteLine("The temperature is " + celsius + " Celsius.");
+
+// int result = Fibonacci(5);
+// Console.WriteLine(result);
+
+// static int Fibonacci(int n)
+// {
+//     int n1 = 0;
+//     int n2 = 1;
+//     int sum;
+
+//     for (int i = 2; i <= n; i++)
+//     {
+//         sum = n1 + n2;
+//         n1 = n2;
+//         n2 = sum;
+//     }
+
+//     return n == 0 ? n1 : n2;
+// }
+
+Console.WriteLine("This message is readable by the end user.");
+Trace.WriteLine("This is a trace message when tracing the app.");
+Debug.WriteLine("This is a debug message just for developers.");
