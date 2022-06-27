0bb5b3f5de6e7991af3745503ce32b803e9ed3fa
yjm <yjm123456789yjm@outlook.com>
2022-06-27 08:56:34 +0800

    text_7

--- Program.cs
+++ Program.cs
@@ -21,6 +21,15 @@
 //     return n == 0 ? n1 : n2;
 // }
 
+// Random dice = new Random();
+// int roll = dice.Next(1,7);
+// Console.WriteLine(roll);
+
 Random dice = new Random();
-int roll = dice.Next(1,7);
-Console.WriteLine(roll);
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
