f87417ad9fbd0b45bc687ca0242e7066c9daedb6
yjm <yjm123456789yjm@outlook.com>
2022-06-28 09:24:59 +0800

    text_10

--- Program.cs
+++ Program.cs
@@ -64,11 +64,19 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
     }
 }
 
-if (total >= 15)
+if (total >= 16)
 {
-    Console.WriteLine("You win");
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
 }
 else
 {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a kitten!");
 }
